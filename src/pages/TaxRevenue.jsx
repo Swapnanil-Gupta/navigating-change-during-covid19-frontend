cacb155574d661b0d127259993cedfee16695f44
vijayrao110125 <vijayrao110125@gmail.com>
2023-11-28 19:54:39 -0500

    tax-analysis

--- src/pages/TaxRevenue.jsx
+++ src/pages/TaxRevenue.jsx
@@ -106,20 +106,20 @@ export default function TaxRevenue() {
       <div className="flex flex-col gap-y-4">
         <h3 className="font-semibold text-2xl">Trend Analysis</h3>
         <div className="flex flex-col gap-y-2">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere illo
-            labore sit delectus exercitationem? Fugiat, distinctio dolore maxime
-            totam inventore nostrum voluptate laboriosam quibusdam alias aliquam
-            tempora nulla, doloribus pariatur iste labore neque suscipit!
-            Voluptate atque, natus cumque maiores nostrum dolor consectetur?
-            Temporibus enim error aliquam consequuntur eos saepe, nihil quis
-            tenetur harum unde quia, rerum fuga. Laboriosam rem aperiam cumque
-            dicta nostrum id culpa corporis soluta mollitia unde facere atque
-            incidunt, minus, explicabo facilis similique? Tenetur impedit
-            maiores id error dicta aliquam sit illo eos nihil eveniet, provident
-            aperiam culpa facilis? Debitis voluptatibus ipsa voluptas deserunt,
-            adipisci velit aliquam.
-          </p>
+        <p>
+          <strong>
+          How does the number of confirmed COVID-19 cases in a state affect the total tax revenue per tax category of that state over the course of 2019-2022? Which tax category saw dramatic changes?
+          </strong></p>
+          <p>Several different trends were observed in the generation of this query and the most significant was Tobacco Products Sales this has significant variability over the years in the percentage change of total revenue from the tobacco products sales tax. The fluctuations are quite pronounced, with a notable peaks between 2010 and 2018 in almost every state and a sharp drop in 2019 and 2020 in most of the states. This drop in 2019 and 2020 could be related to the COVID-19 pandemic's impact on consumer behavior or the economy as a whole.
+          The revenue from alcoholic beverages sales tax, shows significant annual variability in most states. There is a notable peaks until 2017, followed by a decline between 2018 and 2020 in atleast three fourth of the states, there is a considerable drop in the percentage change of revenue, suggesting a negative impact, potentially due to the economic consequences of the COVID-19 pandemic.
+          The percentage change of revenue from insurance premiums sales tax during the onset of the COVID-19 pandemic, depicted by a sharp upward trajectory in the number of confirmed cases starting in 2019. This downward trend in revenue could be influenced by a combination of the economic slowdown, with individuals and businesses cutting costs, including on insurance; a change in risk behaviors leading to potentially reduced insurance policy purchases; insurers possibly offering premiums adjustments or rebates; government interventions such as moratoriums on policy cancellations; and an increase in claims like business interruptions. The alignment of the decline in sales tax revenue with the rise in COVID-19 cases suggests a correlation where the pandemic's broader economic effects impacted the insurance sector's financial outcomes.
+             </p>
+             <p>
+              The next major trend that saw a shift during covid 19 is Corporation net income taxes this reflects varied impacts of the pandemic across different states in the U.S. Likely due to diverse economic structures, state-specific policy responses, sector-based corporate profitability, changes in tax payment timelines, and the unequal distribution of federal aid. States with robust tech or healthcare sectors may have seen tax revenue growth, while those reliant on more affected industries such as tourism faced declines. Additionally, government interventions, both at state and federal levels, including tax relief and stimulus measures, would have influenced these trends, contributing to the observed increases in some states and decreases in others during the pandemic. Motor Fuels Sales Tax this trend has seen various fluctuations over the years
+              but during Covid it has decreased in some states and increased in some other the reasons could be due to Mobility Restrictions, State Tax Policies, Differing Pandemic Impact, Reduced Travel, Supply Chain Disruptions etc.
+             </p>
+             <p>Lastly we can observer that Property Taxes either decreased or remained the same in most of the states because of Economic Downturn, Tax Relief Measures, Property Sales Slowdown, Assessment Delays, Payment Extensions and Penalties Waiving etc. It observable that public utilities licenses tax has decreased or remained constant in three fourth of the states becaues of Reduction in Business Operations, Delayed Payments, Decreased Consumption etc during pandemic. There are several other tax categories that needed to be accessed from the above tax categories shown on the plots to get conclusons about the trends. During pamdemic we can observe that most of the tax categories decreased or remained constant in most of the states. 
+             </p>
         </div>
       </div>
     </main>
